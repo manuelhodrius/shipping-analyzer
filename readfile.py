7575f64f3b6b4f1413d2a0fdb6adac27f55b24e5
Manuel Hodrius <manuel@hodrius.com>
2018-01-24 20:57:36 -0800

    small changes

--- readfile.py
+++ readfile.py
@@ -8,8 +8,20 @@ def connect():
     return c
 
 c = connect()
+droplimit = 0.04
 
 #def give(rownum)
 #for row in c.execute('SELECT counter, timestamp FROM loggerdata LIMIT 1 OFFSET 5'):
-for row in c.execute('SELECT counter, timestamp, x, y FROM loggerdata WHERE x < 0.04 AND y < 0.04'):
+
+#find drops
+for row in c.execute("SELECT counter, timestamp, x, y FROM loggerdata WHERE x < ? AND y < ? AND z < ?", (droplimit,droplimit,droplimit,)):
         print row
+        
+#find not drops
+#for row in c.execute("SELECT timestamp FROM loggerdata WHERE x < ? AND y < ?", (droplimit,droplimit,)):
+#        print row
+
+c.execute("SELECT counter, timestamp FROM loggerdata WHERE x < ? AND y < ?", (droplimit,droplimit,))
+print [float(record[0]) for record in c.fetchall()]
+
+
